ec8d9a4ad1f217280936b34e5ffdc730c489d08f
kaiodeodato <115493943+kaiodeodato@users.noreply.github.com>
2023-03-07 19:43:43 +0000

    delete console.log
    delete console.log

--- src/supabase.js
+++ src/supabase.js
@@ -3,9 +3,5 @@ import { createClient } from "@supabase/supabase-js";
 const supabase_url= process.env.REACT_APP_SUPABASE_URL
 const supabase_key= process.env.REACT_APP_SUPABASE_KEY
 
-<<<<<<< HEAD
-=======
-console.log(supabase_url)
->>>>>>> 83f2932e492bdd7d6f396c20b3714d93d5bac6a8
 
 export const supabase = createClient(supabase_url,supabase_key)
